025bbc42e718877a96b56bcdc2f2aa7aab927253
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-05 23:16:22 +0900

    fix: 블럭 도메인 GET API 인프라스트럭처 영역과 의존 제거 및 POST API 생성

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/web/controller/BlockController.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/web/controller/BlockController.java
@@ -1,42 +1,69 @@
 package com.blockpage.blockservice.adaptor.web.controller;
 
-import com.blockpage.blockservice.adaptor.infrastructure.entity.BlockEntity;
-import com.blockpage.blockservice.adaptor.infrastructure.repository.BlockRepository;
-
+import com.blockpage.blockservice.adaptor.web.apispec.ApiWrapperResponse;
+import com.blockpage.blockservice.adaptor.web.apispec.BlockRequest;
 import com.blockpage.blockservice.adaptor.web.view.MemberBlockView;
-import java.util.List;
-import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/mock/v1/blocks")
+@RequestMapping("/v1/blocks")
 public class BlockController {
 
-    private final BlockRepository blockRepository;
-
     /**
-     * 해당 유저의 블럭 조회 API
+     * Mock Data 작업중 (서비스 로직 없음)
      */
     @GetMapping
-    public ResponseEntity getMemberBlocks() {
-        Long memberId = 1L;
-        List<BlockEntity> blocks = blockRepository.findByMemberId(memberId);
-        Optional<Integer> totalBlocks = (Optional<Integer>) blocks.stream()
-            .filter(BlockEntity::getBlockValidate)
-            .map(BlockEntity::getBlockQuantity)
-            .reduce(Integer::sum);
-        if (totalBlocks.isPresent()) {
-            MemberBlockView result = MemberBlockView.toViewFromMemberBlocks(memberId, totalBlocks.get());
-            return ResponseEntity.status(HttpStatus.OK)
-                .body(result);
-        }
+    public ResponseEntity<ApiWrapperResponse> getMemberBlocks() {
         return ResponseEntity.status(HttpStatus.OK)
-            .body("DB에 값이 없습니다.");
+            .body(new ApiWrapperResponse(new MemberBlockView(1L, 5000)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiWrapperResponse> postBlocks(@RequestParam String type, @RequestBody BlockRequest blockRequest) {
+        switch (type) {
+            case "attendance": {
+                //attendance 내역 생성 포트 + DTO 매핑 함수
+                System.out.println("purchaseRequest : attendance = " + blockRequest.toString());
+            }
+            break;
+            case "charge": {
+                //charge 내역 생성 포트 + DTO 매핑 함수
+                System.out.println("purchaseRequest : charge = " + blockRequest.toString());
+            }
+            break;
+
+            case "refund": {
+                //refund 내역 생성 포트 + DTO 매핑 함수
+                System.out.println("purchaseRequest : refund = " + blockRequest.toString());
+            }
+            break;
+            case "episode-bm": {
+                //episode-bm 내역 생성 포트 + DTO 매핑 함수
+                System.out.println("purchaseRequest : episode-bm = " + blockRequest.toString());
+            }
+            break;
+            case "nft": {
+                //nft 내역 생성 포트 + DTO 매핑 함수
+                System.out.println("purchaseRequest : nft = " + blockRequest.toString());
+            }
+            break;
+            case "profile-skin": {
+                //nft 내역 생성 포트 + DTO 매핑 함수
+                System.out.println("purchaseRequest : profile-skin = " + blockRequest.toString());
+            }
+            break;
+        }
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiWrapperResponse("리소스가 정상적으로 생성되었습니다."));
     }
 }
