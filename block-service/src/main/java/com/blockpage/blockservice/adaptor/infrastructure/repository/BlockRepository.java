912df4dd9834b4565b44063c63673116a20f0344
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-05 22:45:53 +0900

    feat: 멤버 자신의 Block(화페)를 불러오는 메서드 추가

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/repository/BlockRepository.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/repository/BlockRepository.java
@@ -1,8 +1,10 @@
 package com.blockpage.blockservice.adaptor.infrastructure.repository;
 
 import com.blockpage.blockservice.adaptor.infrastructure.entity.BlockEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlockRepository extends JpaRepository<BlockEntity, Long> {
 
+    List<BlockEntity> findByMemberId(Long memberId);
 }
