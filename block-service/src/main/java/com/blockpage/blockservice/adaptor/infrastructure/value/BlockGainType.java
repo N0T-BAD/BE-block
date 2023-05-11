0bccc7d07a02fc237b3d194c5ebc285e3366a900
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-11 17:36:07 +0900

    feat: 블럭 휙득 방법 영어로 명세 수정 및 편의 메서드 추가

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/value/BlockGainType.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/value/BlockGainType.java
@@ -1,15 +1,23 @@
 package com.blockpage.blockservice.adaptor.infrastructure.value;
 
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum BlockGainType {
-    CASH(0, "현금"),
-    GAME(1, "게임"),
-    ATTENDANCE(2, "출석"),
+    CASH(0, "cash"),
+    GAME(1, "game"),
+    ATTENDANCE(2, "attendance"),
     ;
     private int key;
     private String value;
+
+    public static BlockGainType findTypeByValue(String value) {
+        return Arrays.stream(BlockGainType.values())
+            .filter(t -> t.getValue().equals(value))
+            .findFirst()
+            .get();
+    }
 }
