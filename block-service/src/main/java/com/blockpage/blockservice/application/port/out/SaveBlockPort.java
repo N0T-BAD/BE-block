4a9aaf83c508a033074d983b20d1d6cdcd8fd634
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-11 19:47:25 +0900

    fix: CRUD 유스케이스 통합

--- block-service/src/main/java/com/blockpage/blockservice/application/port/out/SaveBlockPort.java
+++
@@ -1,9 +0,0 @@
-package com.blockpage.blockservice.application.port.out;
-
-import com.blockpage.blockservice.application.port.out.FindBlockPort.BlockEntityDto;
-import com.blockpage.blockservice.domain.Block;
-
-public interface SaveBlockPort {
-
-    BlockEntityDto saveBlock(Block block);
-}
