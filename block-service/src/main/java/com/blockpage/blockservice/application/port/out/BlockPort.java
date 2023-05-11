086c6bc562381fbb6a1b053acd2e1fdce408598a
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-11 19:45:11 +0900

    fix: dto에 있던 비즈니스 로직 이동

--- block-service/src/main/java/com/blockpage/blockservice/application/port/out/BlockPort.java
+++ block-service/src/main/java/com/blockpage/blockservice/application/port/out/BlockPort.java
@@ -1,15 +1,20 @@
 package com.blockpage.blockservice.application.port.out;
 
 import com.blockpage.blockservice.adaptor.infrastructure.entity.BlockEntity;
-import com.blockpage.blockservice.application.port.in.BlockQuery.FindQuery;
+import com.blockpage.blockservice.adaptor.infrastructure.value.BlockGainType;
+import com.blockpage.blockservice.application.port.in.BlockUseCase.FindBlockQuery;
+import com.blockpage.blockservice.application.port.in.BlockUseCase.UpdateBlockQuery;
+import com.blockpage.blockservice.domain.Block;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
-public interface FindBlockPort {
+public interface BlockPort {
 
-    List<BlockEntityDto> getMemberBlock(FindQuery findQuery);
+    List<BlockEntityDto> getMemberBlock(Long memberId);
+    BlockEntityDto saveBlock(Block block);
+    List<BlockEntityDto> updateBlock(Block block);
 
     @Builder
     @Getter
@@ -20,16 +25,14 @@ public interface FindBlockPort {
         private Integer blockQuantity;
         private Boolean blockValidate;
         private LocalDateTime expiredDate;
-
-        public boolean isExpired() {
-            return this.expiredDate.isAfter(LocalDateTime.now());
-        }
+        private BlockGainType blockGainType;
 
         public static BlockEntityDto toDto(BlockEntity blockEntity) {
             return BlockEntityDto.builder()
                 .id(blockEntity.getId())
                 .memberId(blockEntity.getMemberId())
                 .blockQuantity(blockEntity.getBlockQuantity())
+                .blockGainType(blockEntity.getBlockGainType())
                 .blockValidate(blockEntity.getBlockValidate())
                 .expiredDate(blockEntity.getExpiredDate())
                 .build();
--- block-service/src/main/java/com/blockpage/blockservice/domain/Block.java
+++ block-service/src/main/java/com/blockpage/blockservice/domain/Block.java
@@ -1,8 +1,9 @@
 package com.blockpage.blockservice.domain;
 
-import com.blockpage.blockservice.adaptor.infrastructure.value.BlockGainType;
 import com.blockpage.blockservice.application.port.in.BlockUseCase.ChargeBlockQuery;
+import com.blockpage.blockservice.application.port.out.BlockPort.BlockEntityDto;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,18 +15,53 @@ public class Block {
 
     private Long memberId;
     private Integer blockQuantity;
-    private BlockGainType blockGainType;
+    private GainType blockGainType;
     private Boolean blockValidate;
     private LocalDateTime expiredDate;
 
+    public boolean isExpired() {
+        return this.expiredDate.isAfter(LocalDateTime.now());
+    }
 
-    public static Block initBlock(ChargeBlockQuery query) {
+
+    public static Block initBlockForCharge(ChargeBlockQuery query) {
         return Block.builder()
             .memberId(query.getMemberId())
-            .blockGainType(BlockGainType.findTypeByValue(query.getType()))
+            .blockGainType(GainType.findByValue(query.getType()))
             .blockQuantity(query.getBlockQuantity())
             .blockValidate(Boolean.TRUE)
             .expiredDate(LocalDateTime.now().plusYears(EXPIRED_BLOCK_YEAR))
             .build();
     }
+
+    public static Block initBlockFromEntityDto(BlockEntityDto dto) {
+        return Block.builder()
+            .memberId(dto.getMemberId())
+            .blockGainType(GainType.findByValue(dto.getBlockGainType().getValue()))
+            .blockQuantity(dto.getBlockQuantity())
+            .blockValidate(dto.getBlockValidate())
+            .expiredDate(dto.getExpiredDate())
+            .build();
+    }
+
+    enum GainType {
+        CASH("cash"), GAME("game"), ATTENDANCE("attendance");
+        private String value;
+
+        GainType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public static GainType findByValue(String value) {
+            return Arrays.stream(GainType.values())
+                .filter(t -> t.getValue().equals(value))
+                .findFirst()
+                .get();
+        }
+    }
+
 }
