75034a7baf64ea155d79c306bc40fea82018cde6
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-11 14:45:55 +0900

    fix: spring 버전 2.7.11로 변경

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/entity/BlockEntity.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/entity/BlockEntity.java
@@ -1,13 +1,13 @@
 package com.blockpage.blockservice.adaptor.infrastructure.entity;
 
 import com.blockpage.blockservice.adaptor.infrastructure.value.BlockGainType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 
 @Getter
