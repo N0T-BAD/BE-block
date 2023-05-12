9bf7f6980f1e1814b14d1b69b71573c096f298e3
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-13 01:38:01 +0900

    feat: 결제 승인 Parameter와 Response 생성

---
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/external/kakao/requestbody/KakaoPayApprovalParams.java
@@ -0,0 +1,30 @@
+package com.blockpage.blockservice.adaptor.external.kakao.requestbody;
+
+import com.blockpage.blockservice.application.port.in.BlockUseCase.KakaoApproveQuery;
+import com.blockpage.blockservice.application.service.BlockService.PaymentOutDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class KakaoPayApprovalParams {
+
+    private String cid;
+    private String tid;
+    private String pg_token;
+    private String partner_order_id;
+    private String partner_user_id;
+
+    public static KakaoPayApprovalParams addEssentialParams(KakaoApproveQuery query, PaymentOutDto receipt) {
+
+        return KakaoPayApprovalParams.builder()
+            .cid("TC0ONETIME")
+            .tid(receipt.getTid())
+            .partner_order_id(receipt.getOrderId())
+            .pg_token(query.getPgToken())
+            .partner_user_id(query.getMemberId().toString())
+            .build();
+    }
+}
