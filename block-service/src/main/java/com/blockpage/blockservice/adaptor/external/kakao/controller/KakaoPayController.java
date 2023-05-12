fe90fc11d6afbbbb8b53b9cc5e2d161aac0da301
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-13 01:42:18 +0900

    feat: 카카오 결제 승인 api 추가

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/external/kakao/controller/KakaoPayController.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/external/kakao/controller/KakaoPayController.java
@@ -1,8 +1,11 @@
 package com.blockpage.blockservice.adaptor.external.kakao.controller;
 
+import com.blockpage.blockservice.adaptor.external.kakao.requestbody.KakaoPayApprovalParams;
 import com.blockpage.blockservice.adaptor.external.kakao.requestbody.KakaoPayReadyParams;
+import com.blockpage.blockservice.adaptor.external.kakao.response.KakaoPayApprovalResponse;
 import com.blockpage.blockservice.adaptor.external.kakao.response.KakaoPayReadyResponse;
 import com.blockpage.blockservice.application.port.out.PaymentPort;
+import com.blockpage.blockservice.application.service.BlockService.KakaoPayApproveDto;
 import com.blockpage.blockservice.application.service.BlockService.KakaoPayReadyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -17,5 +20,11 @@ public class KakaoPayController implements PaymentPort {
     public KakaoPayReadyDto ready(KakaoPayReadyParams kakaoPayReadyParams) {
         KakaoPayReadyResponse response = kakaoPayOpenFeign.ready(kakaoPayReadyParams);
         return new KakaoPayReadyDto(response);
+    }
+
+    @Override
+    public KakaoPayApproveDto approval(KakaoPayApprovalParams kakaoPayApprovalParams) {
+        KakaoPayApprovalResponse response = kakaoPayOpenFeign.approval(kakaoPayApprovalParams);
+        return new KakaoPayApproveDto(response);
     }
 }
