36199e64e0b5f21e3db8957c1156af43fc3ba87f
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-13 02:16:11 +0900

    rename: 어댑터 및 포트 이름들을 명확하게 변경

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/external/kakao/controller/KakaoPayController.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/external/kakao/controller/KakaoPayController.java
@@ -4,7 +4,7 @@ import com.blockpage.blockservice.adaptor.external.kakao.requestbody.KakaoPayApp
 import com.blockpage.blockservice.adaptor.external.kakao.requestbody.KakaoPayReadyParams;
 import com.blockpage.blockservice.adaptor.external.kakao.response.KakaoPayApprovalResponse;
 import com.blockpage.blockservice.adaptor.external.kakao.response.KakaoPayReadyResponse;
-import com.blockpage.blockservice.application.port.out.PaymentPort;
+import com.blockpage.blockservice.application.port.out.PaymentRequestPort;
 import com.blockpage.blockservice.application.service.BlockService.KakaoPayApproveDto;
 import com.blockpage.blockservice.application.service.BlockService.KakaoPayReadyDto;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class KakaoPayController implements PaymentPort {
+public class KakaoPayController implements PaymentRequestPort {
 
     private final KakaoPayOpenFeign kakaoPayOpenFeign;
 
