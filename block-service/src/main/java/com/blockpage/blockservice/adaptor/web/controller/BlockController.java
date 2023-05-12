d755c439f62707b7ac07357320bb8cdcdc44cbb2
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-12 12:13:18 +0900

    feat: 카카오 결제 요청 및 승인 컨트롤러 생성

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/web/controller/BlockController.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/web/controller/BlockController.java
@@ -1,14 +1,21 @@
 package com.blockpage.blockservice.adaptor.web.controller;
 
+import static com.blockpage.blockservice.application.port.in.BlockUseCase.*;
+
 import com.blockpage.blockservice.adaptor.web.view.ApiResponse;
 import com.blockpage.blockservice.adaptor.web.requestbody.BlockRequest;
 import com.blockpage.blockservice.adaptor.web.view.BlockView;
 
+import com.blockpage.blockservice.adaptor.web.view.KakaoApproveView;
+import com.blockpage.blockservice.adaptor.web.view.KakaoReadyView;
 import com.blockpage.blockservice.application.port.in.BlockUseCase;
 import com.blockpage.blockservice.application.port.in.BlockUseCase.ChargeBlockQuery;
 import com.blockpage.blockservice.application.port.in.BlockUseCase.FindBlockQuery;
+import com.blockpage.blockservice.application.port.in.BlockUseCase.KakaoApproveQuery;
 import com.blockpage.blockservice.application.port.in.BlockUseCase.UpdateBlockQuery;
 import com.blockpage.blockservice.application.service.BlockService.BlockQueryDto;
+import com.blockpage.blockservice.application.service.BlockService.KakaoPayApproveDto;
+import com.blockpage.blockservice.application.service.BlockService.KakaoPayReadyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +33,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlockController {
 
     private long MEMBER_TEST_ID = 1L;
+    private String KAKAO_PAY = "kakaopay";
+    private String KAKAO_PAY_READY = "ready";
+    private String KAKAO_PAY_APPROVE = "approve";
 
     private final BlockUseCase blockUseCase;
 
@@ -38,16 +48,40 @@ public class BlockController {
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse<String>> postBlocks(@RequestParam String type, @RequestBody BlockRequest blockRequest) {
-        blockUseCase.createBlock(new ChargeBlockQuery(MEMBER_TEST_ID, type, blockRequest.getBlockQuantity()));
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(new ApiResponse("블럭이 생성되었습니다."));
+    public ResponseEntity<ApiResponse<?>> postBlocks(
+        @RequestParam String type,
+        @RequestParam(required = false) String corp,
+        @RequestParam(required = false) String step,
+        @RequestBody BlockRequest request
+    ) {
+        if (corp == null) {
+            blockUseCase.createBlock(new ChargeBlockQuery(MEMBER_TEST_ID, type, request.getQuantity()));
+            return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse("성공"));
+
+        } else if (corp.equals(KAKAO_PAY) && step.equals(KAKAO_PAY_READY)) {
+            KakaoPayReadyDto kakaoPayReadyDto = blockUseCase.kakaoPayReady(
+                new KakaoReadyQuery(MEMBER_TEST_ID, request.getItemName(), request.getQuantity(),
+                    request.getTotalAmount()));
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse(new KakaoReadyView(kakaoPayReadyDto)));
+
+        } else if (corp.equals(KAKAO_PAY) && step.equals(KAKAO_PAY_APPROVE)) {
+            KakaoPayApproveDto kakaoPayReadyDto = blockUseCase.kakaoPayApprove(new KakaoApproveQuery(MEMBER_TEST_ID, request.getPgToken()));
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse(new KakaoApproveView(kakaoPayReadyDto)));
+
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponse("실패"));
+        }
     }
 
     @PatchMapping
     public ResponseEntity<ApiResponse<String>> patchBlocks(@RequestBody BlockRequest blockRequest) {
-        blockUseCase.updateBlock(new UpdateBlockQuery(MEMBER_TEST_ID, blockRequest.getBlockQuantity()));
+
+        blockUseCase.updateBlock(new UpdateBlockQuery(MEMBER_TEST_ID, blockRequest.getQuantity()));
         return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiResponse("블럭이 생성되었습니다."));
+            .body(new ApiResponse("성공"));
     }
 }
