165c4363878eeeaedc454fb246aa109486918113
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-14 00:17:00 +0900

    feat: 주문번호로 결제내역 불러오는 기능 추가

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/mysql/repository/PaymentRepository.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/mysql/repository/PaymentRepository.java
@@ -1,8 +1,10 @@
 package com.blockpage.blockservice.adaptor.infrastructure.mysql.repository;
 
 import com.blockpage.blockservice.adaptor.infrastructure.mysql.entity.PaymentEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
 
+    Optional<PaymentEntity> findByOrderId(String orderId);
 }
