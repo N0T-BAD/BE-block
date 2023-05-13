cd73e4cd98f21dad20088631b0150465fa3b6eb6
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-14 00:15:17 +0900

    feat: 결제 취소 함수 paymentRequestPort에 명세

--- block-service/src/main/java/com/blockpage/blockservice/application/port/out/PaymentRequestPort.java
+++ block-service/src/main/java/com/blockpage/blockservice/application/port/out/PaymentRequestPort.java
@@ -2,11 +2,14 @@ package com.blockpage.blockservice.application.port.out;
 
 import com.blockpage.blockservice.adaptor.external.kakao.requestbody.KakaoPayApprovalParams;
 import com.blockpage.blockservice.adaptor.external.kakao.requestbody.KakaoPayReadyParams;
+import com.blockpage.blockservice.adaptor.external.kakao.requestbody.KakaoPayRefundParams;
 import com.blockpage.blockservice.application.service.BlockService.KakaoPayApproveDto;
 import com.blockpage.blockservice.application.service.BlockService.KakaoPayReadyDto;
+import com.blockpage.blockservice.application.service.BlockService.KakaoPayRefundDto;
 
 public interface PaymentRequestPort {
 
     KakaoPayReadyDto ready(KakaoPayReadyParams kakaoPayReadyParams);
     KakaoPayApproveDto approval(KakaoPayApprovalParams kakaoPayApprovalParams);
+    KakaoPayRefundDto cancel(KakaoPayRefundParams kakaoPayRefundParams);
 }
