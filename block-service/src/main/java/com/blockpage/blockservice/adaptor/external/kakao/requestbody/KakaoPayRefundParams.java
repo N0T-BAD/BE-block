534782c992d4bff03d9e68573344c3396ddff934
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-14 00:11:36 +0900

    feat: 카카오 환불 Response Spec 생성

---
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/external/kakao/requestbody/KakaoPayRefundParams.java
@@ -0,0 +1,26 @@
+package com.blockpage.blockservice.adaptor.external.kakao.requestbody;
+
+import com.blockpage.blockservice.application.port.out.PaymentPersistencePort.PaymentEntityDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class KakaoPayRefundParams {
+
+    private String cid;
+    private String tid;
+    private Integer cancel_amount;
+    private Integer cancel_tax_free_amount;
+
+    public static KakaoPayRefundParams addEssentialParams(PaymentEntityDto dto) {
+        return KakaoPayRefundParams.builder()
+            .cid("TC0ONETIME")
+            .cancel_tax_free_amount(0)
+            .tid(dto.getTid())
+            .cancel_amount(dto.getTotalAmount())
+            .build();
+    }
+}
