d8c5eee8f2a857f854b2003c56db8f28c9713d12
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-13 20:11:33 +0900

    feat: 결제 레포지토리 저장 포트 및 결제 레포지토리 어댑터 생성

---
+++ block-service/src/main/java/com/blockpage/blockservice/application/port/out/PaymentPersistencePort.java
@@ -0,0 +1,44 @@
+package com.blockpage.blockservice.application.port.out;
+
+import com.blockpage.blockservice.adaptor.infrastructure.mysql.entity.PaymentEntity;
+import com.blockpage.blockservice.adaptor.infrastructure.mysql.value.PaymentType;
+import com.blockpage.blockservice.application.service.BlockService.PaymentDto;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+public interface PaymentPersistencePort {
+
+    PaymentEntityDto savePaymentRecord(PaymentDto paymentDto);
+
+    @Builder
+    @Getter
+    class PaymentEntityDto {
+
+        private Long id;
+        private Long memberId;
+        private String orderId;
+        private String tid;
+        private String itemName;
+        private Integer blockQuantity;
+        private PaymentType paymentType;
+        private LocalDateTime paymentTime;
+        private String paymentCompany;
+        private Integer totalAmount;
+
+        public static PaymentEntityDto toDto(PaymentEntity paymentEntity) {
+            return PaymentEntityDto.builder()
+                .id(paymentEntity.getId())
+                .memberId(paymentEntity.getMemberId())
+                .orderId(paymentEntity.getOrderId())
+                .tid(paymentEntity.getTid())
+                .itemName(paymentEntity.getItemName())
+                .blockQuantity(paymentEntity.getBlockQuantity())
+                .paymentType(paymentEntity.getPaymentType())
+                .paymentTime(paymentEntity.getPaymentTime())
+                .paymentCompany(paymentEntity.getPaymentCompany())
+                .totalAmount(paymentEntity.getTotalAmount())
+                .build();
+        }
+    }
+}
