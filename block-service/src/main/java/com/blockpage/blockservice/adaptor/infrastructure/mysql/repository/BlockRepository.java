008bc12b1bee987f686fedf9680c6cd2d8ce24a0
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-14 00:16:44 +0900

    feat: 주문번호로 블럭 내역 불러오고 삭제하는기능추가

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/mysql/repository/BlockRepository.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/mysql/repository/BlockRepository.java
@@ -2,9 +2,14 @@ package com.blockpage.blockservice.adaptor.infrastructure.mysql.repository;
 
 import com.blockpage.blockservice.adaptor.infrastructure.mysql.entity.BlockEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlockRepository extends JpaRepository<BlockEntity, Long> {
 
     List<BlockEntity> findByMemberId(Long memberId);
+
+    Optional<BlockEntity> findByOrderId(String orderId);
+
+    void deleteByOrderId(String orderId);
 }
