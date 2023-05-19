0c3714111caca5b441170d2cba558a968152ce9a
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-19 14:06:53 +0900

    fix: 잘못된 쿼리 수정

--- block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/mysql/persistence/PaymentPersistenceAdaptor.java
+++ block-service/src/main/java/com/blockpage/blockservice/adaptor/infrastructure/mysql/persistence/PaymentPersistenceAdaptor.java
@@ -2,8 +2,9 @@ package com.blockpage.blockservice.adaptor.infrastructure.mysql.persistence;
 
 import com.blockpage.blockservice.adaptor.infrastructure.mysql.entity.PaymentEntity;
 import com.blockpage.blockservice.adaptor.infrastructure.mysql.repository.PaymentRepository;
+import com.blockpage.blockservice.adaptor.infrastructure.mysql.value.BlockGainType;
+import com.blockpage.blockservice.adaptor.infrastructure.mysql.value.BlockLossType;
 import com.blockpage.blockservice.application.port.out.PaymentPersistencePort;
-import com.blockpage.blockservice.application.service.PaymentService;
 import com.blockpage.blockservice.application.service.PaymentService.PaymentEntityDto;
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +18,7 @@ public class PaymentPersistenceAdaptor implements PaymentPersistencePort {
     private final PaymentRepository paymentRepository;
 
     @Override
-    public PaymentEntityDto savePaymentRecord(PaymentService.PaymentEntityDto paymentEntityDto) {
+    public PaymentEntityDto savePaymentRecord(PaymentEntityDto paymentEntityDto) {
         PaymentEntity paymentEntity = paymentRepository.save(PaymentEntity.toEntity(paymentEntityDto));
         return PaymentEntityDto.toDtoFromEntity(paymentEntity);
     }
@@ -30,7 +31,7 @@ public class PaymentPersistenceAdaptor implements PaymentPersistencePort {
 
     @Override
     public List<PaymentEntityDto> getBlockGainType(Long memberId) {
-        List<PaymentEntity> paymentEntityList = paymentRepository.findByMemberIdAndBlockGainTypeIsNotNull(memberId);
+        List<PaymentEntity> paymentEntityList = paymentRepository.findByMemberIdAndBlockGainTypeNot(memberId, BlockGainType.NONE);
         return paymentEntityList.stream()
             .map(PaymentEntityDto::toDtoFromEntity)
             .toList();
@@ -38,7 +39,7 @@ public class PaymentPersistenceAdaptor implements PaymentPersistencePort {
 
     @Override
     public List<PaymentEntityDto> getBlockLossType(Long memberId) {
-        List<PaymentEntity> paymentEntityList = paymentRepository.findByMemberIdAndBlockLossTypeIsNotNull(memberId);
+        List<PaymentEntity> paymentEntityList = paymentRepository.findByMemberIdAndBlockLossTypeNot(memberId, BlockLossType.NONE);
         return paymentEntityList.stream()
             .map(PaymentEntityDto::toDtoFromEntity)
             .toList();
--- block-service/src/main/java/com/blockpage/blockservice/application/port/out/PaymentPersistencePort.java
+++ block-service/src/main/java/com/blockpage/blockservice/application/port/out/PaymentPersistencePort.java
@@ -1,11 +1,10 @@
 package com.blockpage.blockservice.application.port.out;
 
-import com.blockpage.blockservice.application.service.PaymentService;
 import com.blockpage.blockservice.application.service.PaymentService.PaymentEntityDto;
 import java.util.List;
 public interface PaymentPersistencePort {
 
-    PaymentEntityDto savePaymentRecord(PaymentService.PaymentEntityDto paymentEntityDto);
+    PaymentEntityDto savePaymentRecord(PaymentEntityDto paymentEntityDto);
     PaymentEntityDto getPaymentByOrderId(String orderId);
     List<PaymentEntityDto> getBlockLossType(Long memberId);
     List<PaymentEntityDto> getBlockGainType(Long memberId);
