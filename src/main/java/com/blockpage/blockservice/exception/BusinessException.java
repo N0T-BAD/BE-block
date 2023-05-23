b6758bbb5d78f275b823578a75762767fcd27d0e
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-23 20:25:03 +0900

    feat: Business 예외처리 함수 정의

---
+++ src/main/java/com/blockpage/blockservice/exception/BusinessException.java
@@ -0,0 +1,24 @@
+package com.blockpage.blockservice.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BusinessException extends RuntimeException {
+
+    private String message;
+    private HttpStatus httpStatus;
+
+    public BusinessException(String message, HttpStatus httpStatus) {
+        super();
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
